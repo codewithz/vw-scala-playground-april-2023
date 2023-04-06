098cb6f20cec0c7db3eb3925531fb3bad9209861
Zartab M Nakhwa <zartab@codewithz.com>
2023-04-06 10:02:49 +0530

    Private and Setter Getters

--- object-orientation/Account.scala
+++ object-orientation/Account.scala
@@ -3,15 +3,24 @@ import java.time.LocalDateTime
 
 class Account(id:UUID,name:String,dateOpened:LocalDateTime) {
 
-    var _id:UUID=id
-    var _name:String=name
-    var _dateOpened:LocalDateTime=dateOpened
+    private var _id:UUID=id
+    private var _name:String=name
+    private var _dateOpened:LocalDateTime=dateOpened
 
     //Auxiliary Constructor 
 
-    def this(name:String){
+    def this(name:String)={
         this(UUID.randomUUID(),name,LocalDateTime.now)
     }
+
+    def getId:UUID = _id
+    def getName:String =_name
+    def getDateOpened:LocalDateTime=_dateOpened
+
+    def setName(newName:String):Unit = {
+        _name=newName
+    }
+
   
 }
 
@@ -21,8 +30,14 @@ object AccountRunner extends App{
     val a2=new Account(UUID.randomUUID(),"Account 2",LocalDateTime.now())
     val a3=new Account("Account 3")
 
-    println(a1._id,a1._name,a1._dateOpened)
-    println(a2._id,a2._name,a2._dateOpened)
-    println(a3._id,a3._name,a3._dateOpened)
+    println(a1.getId,a1.getName,a1.getDateOpened)
+    println(a2.getId,a2.getName,a2.getDateOpened)
+    println(a3.getId,a3.getName,a3.getDateOpened)
+
+    // a1._id=UUID.randomUUID()
+    // a1._name="Updated Account 01"
+    a1.setName("Updated Account 1")
+    println(a1.getId,a1.getName,a1.getDateOpened)
+    
 
 }
