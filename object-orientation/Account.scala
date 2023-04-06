1e3910f4343152e6bceb8e654de726d4942e0f6b
Zartab M Nakhwa <zartab@codewithz.com>
2023-04-06 12:39:31 +0530

    Traits: Stackable Modifications

--- object-orientation/Account.scala
+++ object-orientation/Account.scala
@@ -42,6 +42,29 @@ class DepositAccount(name:String) extends Account(name:String){
     }
 }
 
+trait Balance {
+    private var _balance:Double=0
+    def getBalance:Double = _balance
+    def setBalance(newBalance:Double):Unit ={
+        _balance=newBalance
+    }
+
+    override def toString(): String = s"Balance =${_balance}"
+}
+
+trait AnnualFees extends Balance{
+    override def setBalance(newBalance: Double): Unit = super.setBalance(newBalance-100)
+}
+
+trait HighSavings extends  Balance {
+    override def setBalance(newBalance: Double): Unit = super.setBalance((newBalance+500)*(1+0.5))
+
+}
+
+class PremiumSavingsAccount(name:String) extends DepositAccount(name) with AnnualFees
+
+class PremiumPromotionalSavingsAccount(name:String) extends DepositAccount(name)  with HighSavings with AnnualFees
+
 
 
 object AccountRunner extends App{
@@ -59,15 +82,36 @@ object AccountRunner extends App{
     // a1.setName("Updated Account 1")
     // println(a1.getId,a1.getName,a1.getDateOpened)
 
-    val ca1:Account =new CreditAccount("Travel Mastercard")
-    println(ca1.getId,ca1.getName,ca1.getDateOpened,ca1.getAccountType)
-    println(ca1)
+    // val ca1:Account =new CreditAccount("Travel Mastercard")
+    // println(ca1.getId,ca1.getName,ca1.getDateOpened,ca1.getAccountType)
+    // println(ca1)
 
-    println("---------------------------------------------")
+    // println("---------------------------------------------")
 
-    val da1:DepositAccount=new DepositAccount("Super Savings Account")
-    println(da1.getId,da1.getName,da1.getDateOpened,da1.getAccountType)
-    println(da1)
-    da1.printMe()
+    // val da1:DepositAccount=new DepositAccount("Super Savings Account")
+    // println(da1.getId,da1.getName,da1.getDateOpened,da1.getAccountType)
+    // println(da1)
+    // da1.printMe()
+
+    val a1=new PremiumSavingsAccount("Premium Savings Account")
+    a1.setBalance(999)
+    println(a1.getBalance)
+
+    println("----------------------------------------------")
+
+    val a2=new PremiumPromotionalSavingsAccount("PP S A")
+    a2.setBalance(999)
+    println(a2.getBalance)
+
 
 }
+
+// class C1 
+// class C2 
+// class C3 
+// // class C4 extends C1 with C2 with C3
+
+// trait T1
+// trait T2
+// trait T3
+// trait T4 extends T1 with T2 with T3
